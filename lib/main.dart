cedec614c284cd1e2ddee963e2037ffb85227635
taufiq <tuafiq8214829@gmail.com>
2026-03-16 23:10:50 +0700

    konfigurasi utama

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,23 @@ void main() {
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
-    )
+      statusBarBrightness: Brightness.light,
+    ),
   );
+runApp(const BrimoApp());
+}
+
+class BrimoApp extends StatelessWidget {
+  const BrimoApp({Key? key}) : super(key: key );
+  @override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      fontFamily: 'Roboto',
+      primaryColor: const Color(0xFF005EAA),
+    ),
+    home: const BrimoHomeScreen(),
+  );
+}
 }
